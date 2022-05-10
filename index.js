27110ffddbeabc590f1b17f7c7c616e337130c2f
y-k256 <105215118+y-k256@users.noreply.github.com>
2022-05-10 17:12:21 +0900

    Pages
    --

---
+++ index.js
@@ -0,0 +1,34 @@
+function OnButtonClick(){
+    
+    var target = document.getElementById('output');
+    
+    var From = document.getElementById('pw');
+    
+    if (From.value == 'yusers')
+    {
+        location = 'https://www.google.com';
+    }
+    else
+    {
+        target.innerHTML = '<p style="font-size: 10pt; color: red;">パスワードが違います。やり直してください。</p>';
+    }
+}
+
+function loadTextFile(fName)
+{
+httpObj = createXMLHttpRequest(displayData);
+if (httpObj)
+{
+httpObj.open("GET",fName,true);
+httpObj.send(null);
+}
+}
+function displayData()
+{
+if ((httpObj.readyState == 4) && (httpObj.status == 200))
+{
+pass = httpObj.responseText;
+}else{;
+pass = '';
+}
+}
